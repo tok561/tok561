fadc3b86dbf82232f19b06fff2e67f7bd4f54539
Dieter <aktual@gmx.de>
2024-05-31 18:57:49 +0200

    xaA

---
+++ app/ui/dashboard/page.tsx
@@ -0,0 +1,12 @@
+// app/ui/dashboard/page.tsx
+import React from 'react';
+
+const DashboardPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
+};
+
+export default DashboardPage;
